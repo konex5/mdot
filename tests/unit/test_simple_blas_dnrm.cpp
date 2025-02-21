4e43e57c4da1245e5213baefb010acc1a7d4237a
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    add ddot

--- tests/unit/test_simple_blas_dnrm.cpp
+++ tests/unit/test_simple_blas_dnrm.cpp
@@ -8,7 +8,9 @@ using namespace std;
 #define dnum_t double
 
 extern "C" {
-  dnum_t dnrm2_(const size_t* n,  const dnum_t *a, const size_t* incX);
+dnum_t dnrm2_(const size_t *n, const dnum_t *x, const size_t *incX);
+dnum_t ddot_(const size_t *n, const dnum_t *x, const size_t *incX,
+             const dnum_t *y, const size_t *incY);
 }
 
 #undef size_t
@@ -16,20 +18,28 @@ extern "C" {
 
 BOOST_AUTO_TEST_CASE(test_dnorm_simple) {
 
-  { // real, row major
-    const std::size_t N = 3;
-    const double A[N] = {0, 2, 0};
-    const double B[N] = {-1.2, 1.3, 2.7};
-    
-    double norm_out;
-    const std::size_t incX=1;
+  const std::size_t N = 3;
+  const double A[N] = {0, 2, 0};
+  const double B[N] = {-1.2, 1.3, 2.7};
 
-    norm_out = dnrm2_(&N,A,&incX);
-    
+  { // nrm2
+
+    double norm_out;
+    const std::size_t incX = 1;
+
+    norm_out = dnrm2_(&N, A, &incX);
+
     BOOST_CHECK(norm_out == 2);
-    
-    norm_out = dnrm2_(&N,B,&incX);
+
+    norm_out = dnrm2_(&N, B, &incX);
     BOOST_CHECK(norm_out == 3.228002478313795);
-    
+  }
+  {
+    double norm_square_out;
+    const std::size_t incX = 1;
+    norm_square_out = ddot_(&N, A, &incX, A, &incX);
+    BOOST_CHECK(norm_square_out == 4);
+    norm_square_out = ddot_(&N, B, &incX, B, &incX);
+    BOOST_CHECK(norm_square_out == 10.420000000000002);
   }
 }
