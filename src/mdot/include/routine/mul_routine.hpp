b76306dbbf54ad16062ca98886ffe54179c85e79
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    finish mul_usv_nondeg

--- src/mdot/include/routine/mul_routine.hpp
+++ src/mdot/include/routine/mul_routine.hpp
@@ -110,7 +110,6 @@ void mul_usv_nondeg(std::vector<std::vector<dnum_t>> &array_U,
                               std::get<1>(nondeg_dims[i]), cut[i]};
       m_shape_t shape_right = {cut[i], std::get<2>(nondeg_dims[i]),
                                std::get<3>(nondeg_dims[i])};
-      std::vector<dnum_t> mat_left, mat_right;
       //
       const std::size_t dim0 =
           static_cast<std::size_t>(std::get<0>(shape_left));
@@ -120,59 +119,25 @@ void mul_usv_nondeg(std::vector<std::vector<dnum_t>> &array_U,
           static_cast<std::size_t>(std::get<1>(shape_right));
       const std::size_t dim3 =
           static_cast<std::size_t>(std::get<2>(shape_right));
+      std::vector<dnum_t> mat_left(dim0 * dim1, cut[i]);
+      std::vector<dnum_t> mat_right(cut[i], dim2 * dim3);
+
       if (is_um == 0) {
         for (auto &s : array_S[i])
           s = sqrt(s);
-
+        mul_mat_diag(mat_left, array_U[i], dim0 * dim1, array_S[i].size(),
+                     array_S[i], cut[i]);
+        mul_diag_mat(mat_right, array_V[i], array_S[i].size(), dim2 * dim3,
+                     array_S[i], cut[i]);
       } else if (is_um == 1) {
-
+        mat_left.swap(array_U[i]);
+        mul_diag_mat(mat_right, array_V[i], array_S[i].size(), dim2 * dim3,
+                     array_S[i], cut[i]);
       } else {
+        mul_mat_diag(mat_left, array_U[i], dim0 * dim1, array_S[i].size(),
+                     array_S[i], cut[i]);
+        mat_right.swap(array_V[i]);
       }
-
-      /*
-        for _ in range(len(nondeg)):  # reversed, and passed by pop.
-            Dsi = cut.pop()
-            if Dsi > 0:
-                dims = nondeg_dims.pop()
-                tmp_nondeg = nondeg.pop()
-                if is_um is None:
-                    diag_sqrt = _np.diag(_np.sqrt(array_S.pop()[:Dsi]))
-                    # M
-                    mat_left = _np.dot(array_U.pop()[:, :Dsi],
-        diag_sqrt).reshape( dims[0], dims[1], Dsi
-                    )
-                    # M
-                    mat_right = _np.dot(diag_sqrt, array_V.pop()[:Dsi,
-        :]).reshape( Dsi, dims[2], dims[3]
-                    )
-                elif is_um:
-                    # U
-                    mat_left = array_U.pop()[:, :Dsi].reshape(dims[0], dims[1],
-        Dsi) # M mat_right = _np.dot( _np.diag(array_S.pop()[:Dsi]),
-        array_V.pop()[:Dsi, :]
-                    ).reshape(Dsi, dims[2], dims[3])
-                else:
-                    # M
-                    mat_left = _np.dot(
-                        array_U.pop()[:, :Dsi], _np.diag(array_S.pop()[:Dsi])
-                    ).reshape(dims[0], dims[1], Dsi)
-                    # V
-                    mat_right = array_V.pop()[:Dsi, :].reshape(Dsi, dims[2],
-        dims[3])
-
-                dst_lhs_blocs[
-                    (tmp_nondeg[1][0], tmp_nondeg[1][1], tmp_nondeg[0])
-                ] = mat_left
-                dst_rhs_blocs[
-                    (tmp_nondeg[0], tmp_nondeg[1][2], tmp_nondeg[1][3])
-                ] = mat_right
-            else:
-                array_U.pop()
-                array_V.pop()
-                array_S.pop()
-                nondeg_dims.pop()
-                nondeg.pop()
-                */
       dst_lhs_blocs[{std::get<0>(theta_index), std::get<1>(theta_index),
                      middle_index}] = {shape_left, mat_left};
       dst_rhs_blocs[{middle_index, std::get<2>(theta_index),
