25835a34b87319f5be360d5f1515e7994ada9a44
konex5 <fix@konex.dev>
2025-02-21 14:07:48 +0100

    add none sm sp

--- tests/unit/test_operators.cpp
+++ tests/unit/test_operators.cpp
@@ -4,15 +4,18 @@
 #include "mdot/include/operators.hpp"
 
 inline darr_t mul_twotwo(darr_t a, darr_t b) {
-  darr_t c;
+  darr_t c(4);
   for (int i = 0; i < 2; i++)
-    for (int j = 0; j < 2; j++)
+    for (int j = 0; j < 2; j++) {
+      double sum = 0;
       for (int k = 0; k < 2; k++)
-        c[i + 2 * j] = a[i + 2 * k] * b[k + 2 * j];
+        sum += a[i * 2 + k] * b[k * 2 + j];
+      c[i + 2 * j] = sum;
+    }
   return c;
 }
 
-BOOST_AUTO_TEST_CASE(test_indexdw) {
+BOOST_AUTO_TEST_CASE(test_operators) {
 
   auto shid = real_single_operator("sh-id", "sh-none");
 
@@ -26,4 +29,12 @@ BOOST_AUTO_TEST_CASE(test_indexdw) {
   BOOST_CHECK(shid_square[1 + 0 * 2] == 0);
   BOOST_CHECK(shid_square[1 + 1 * 2] == 1);
   BOOST_CHECK(shid_square[0 + 1 * 2] == 0);
+  //
+  auto shsp = real_single_operator("sh-sp", "sh-none");
+  BOOST_CHECK((shsp[{0, 0}])[1 + 0 * 2] == 1);
+  auto shsm = real_single_operator("sh-sm", "sh-none");
+  BOOST_CHECK((shsm[{0, 0}])[0 + 1 * 2] == 1);
+  //
+  auto shspsm = mul_twotwo(shsp[{0, 0}], shsm[{0, 0}]);
+  BOOST_CHECK(shspsm[0 + 0 * 2] == 1);
 }
