10abde6f45b8a9b58203da0d48ee819a33897a3c
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    continue wip

--- src/mdot/include/routine/interface.hpp
+++ src/mdot/include/routine/interface.hpp
@@ -5,23 +5,22 @@
 
 namespace mdot {
 
-void mm_to_theta_no_gate(
-    dtbloc_t dst_blocs,
-    dmbloc_t lhs_blocs,
-    dmbloc_t rhs_blocs,
-    bool conserve_left_right = false
-) {
-    std::vector<m_index_t> left_indices;
-    for (auto& [key,_]: lhs_blocs)
-        left_indices.push_back(key);
-    std::vector<m_index_t> right_indices;
-    for (auto& [key,_]: rhs_blocs)
-        right_indices.push_back(key);
-    
-    auto about_indices_to_contract = indices_dst_theta_no_gate(left_indices,right_indices,conserve_left_right);
-    /* 
-    mul_mm_blocs(dst_blocs, lhs_blocs, rhs_blocs, dest_indices)
-    */
+void mm_to_theta_no_gate(dtbloc_t dst_blocs, dmbloc_t lhs_blocs,
+                         dmbloc_t rhs_blocs, bool conserve_left_right = false) {
+  std::vector<m_index_t> left_indices;
+  for (auto &[key, _] : lhs_blocs)
+    left_indices.push_back(key);
+  std::vector<m_index_t> right_indices;
+  for (auto &[key, _] : rhs_blocs)
+    right_indices.push_back(key);
+
+  auto about_indices_to_contract =
+      split_degenerate_indices(indices_dst_theta_no_gate(
+          left_indices, right_indices, conserve_left_right));
+
+  // mul_mm_blocs_new(dst_blocs, lhs_blocs, rhs_blocs,
+  // about_indices_to_contract.first) mul_mm_blocs_dup(dst_blocs, lhs_blocs,
+  // rhs_blocs, about_indices_to_contract.second)
 }
-    
-}
+
+} // namespace mdot
--- tests/unit/test_routine_interface.cpp
+++ tests/unit/test_routine_interface.cpp
@@ -7,6 +7,6 @@ BOOST_AUTO_TEST_CASE(test_routine_indices_indices_dst_theta_no_gate) {
   dtbloc_t dst_blocs;
   dmbloc_t lhs_blocs;
   dmbloc_t rhs_blocs;
-  mdot::mm_to_theta_no_gate(dst_blocs,lhs_blocs, rhs_blocs, true);
-  //BOOST_CHECK(std::get<0>(std::get<0>(out[0])) == 0);
+  mdot::mm_to_theta_no_gate(dst_blocs, lhs_blocs, rhs_blocs, true);
+  // BOOST_CHECK(std::get<0>(std::get<0>(out[0])) == 0);
 }
