47c350dbb15de7b430d023ec5171f695b0d2f6f4
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    add sub and sum

--- tests/unit/test_routine_indices.cpp
+++ tests/unit/test_routine_indices.cpp
@@ -11,3 +11,19 @@ BOOST_AUTO_TEST_CASE(test_routine_indices_indices_dst_theta_no_gate) {
   auto pair_out = mdot::split_degenerate_indices(out);
   BOOST_CHECK(std::get<0>(std::get<0>(pair_out.first[0])) == 0);
 }
+
+
+BOOST_AUTO_TEST_CASE(test_routine_indices_indices_sum_sub) {
+  constexpr index_t lhs = 3;
+  constexpr index_t rhs = 1;
+
+  BOOST_CHECK(mdot::internal_qn_sum(lhs,rhs) == 4);
+  BOOST_CHECK(mdot::internal_qn_sub(lhs,rhs) == 2);
+
+  index_t o_lhs = 5;
+  index_t o_rhs = 2;
+
+  BOOST_CHECK(mdot::internal_qn_sum(o_lhs,o_rhs) == 7);
+  BOOST_CHECK(mdot::internal_qn_sub(o_lhs,o_rhs) == 3);
+
+}
