785d455db3017ca8949db175e47ea9a6c66f4a12
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    same coef! Yeah

--- src/mdot/include/routine/mul_routine.hpp
+++ src/mdot/include/routine/mul_routine.hpp
@@ -2,6 +2,8 @@
 
 #include "mdot/include/babel_type.hpp"
 
+#include <iostream>
+
 extern "C" {
 void dgemm_(const char *transa, const char *transb, const size_t *m,
             const size_t *n, const size_t *k, const dnum_t *alpha,
@@ -37,16 +39,28 @@ void mul_mm_blocs_new(
 }
 
 void mul_mm_blocs_dup(
-    dtbloc_t &new_blocks, dmbloc_t old_blocks1, dmbloc_t old_blocks2,
+    dtbloc_t &new_blocs, dmbloc_t lhs_blocs, dmbloc_t rhs_blocs,
     std::vector<std::tuple<t_index_t, m_index_t, m_index_t>> buildtarget) {
-  /*
-  for target, it1, it2 in buildtarget:
-      new_blocks[target] += _np.tensordot(
-          old_blocks1[it1],
-          old_blocks2[it2],
-          axes=(2, 0),
-      )
-  */
-}
+  const double alpha = 1., beta = 0.;
+  for (auto &[target, lhs_it, rhs_it] : buildtarget) {
+    auto N = static_cast<std::size_t>(std::get<0>(lhs_blocs[lhs_it].first) *
+                                      std::get<1>(lhs_blocs[lhs_it].first));
+    auto M = static_cast<std::size_t>(std::get<1>(rhs_blocs[rhs_it].first) *
+                                      std::get<2>(rhs_blocs[rhs_it].first));
+    auto K = static_cast<std::size_t>(std::get<2>(lhs_blocs[lhs_it].first));
+    auto NM = N*M;
+    std::vector<dnum_t> mat_out(NM);
 
+    dgemm_((char *)"N", (char *)"N", &M, &N, &K, &alpha,
+           rhs_blocs[rhs_it].second.data(), &M, lhs_blocs[lhs_it].second.data(),
+           &K, &beta, mat_out.data(), &M);
+
+    #pragma omp
+    for (size_t i =0;i<NM;i++) {
+      new_blocs[target].second[i] += mat_out[i];
+    }
+    std::cout << new_blocs[target].second[0] << " ";
+
+}
+    }
 } // namespace mdot
