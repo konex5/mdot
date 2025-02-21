c7a72fccb9cdf28520f8df4fb42b0efe536d15f3
konex5 <fix@konex.dev>
2025-02-21 14:07:47 +0100

    add typedef for whole project

---
+++ src/mdot/include/typedef.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <array>
+#include <complex>
+#include <vector>
+
+#define dnum_t double
+#define znum_t std::complex<double>
+
+typedef typename std::vector<std::vector<dnum_t>> array_of_s_type;
