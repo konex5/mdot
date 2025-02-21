0f10e17bc35c0d2c40d0ea3859109d6ef4b0dc48
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    add tests

--- tests/unit/test_routine_indices.cpp
+++ tests/unit/test_routine_indices.cpp
@@ -29,8 +29,21 @@ BOOST_AUTO_TEST_CASE(test_routine_indices_indices_sum_sub) {
 BOOST_AUTO_TEST_CASE(test_routine_indices_potential_middle_indices) {
   std::vector<t_index_t> theta_indices = {
       {0, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}};
-  auto middle = mdot::potential_middle_indices(theta_indices, -1);
+  auto middle = mdot::potential_middle_indices(theta_indices, 1);
   BOOST_CHECK(middle[0] == 0);
   BOOST_CHECK(middle[1] == 1);
-  auto out = mdot::degeneracy_in_theta(theta_indices, middle, -1);
+  auto out = mdot::degeneracy_in_theta(theta_indices, middle, 1);
+  BOOST_CHECK(std::get<0>(out.first[0].second)==0);
+  BOOST_CHECK(std::get<1>(out.first[0].second)==1);
+  BOOST_CHECK(std::get<2>(out.first[0].second)==0);
+  BOOST_CHECK(std::get<3>(out.first[0].second)==0);
+  BOOST_CHECK(std::get<0>(out.second[0].second[0])==0);
+  BOOST_CHECK(std::get<1>(out.second[0].second[0])==0);
+  BOOST_CHECK(std::get<2>(out.second[0].second[0])==0);
+  BOOST_CHECK(std::get<3>(out.second[0].second[0])==0);
+  BOOST_CHECK(std::get<0>(out.second[0].second[1])==0);
+  BOOST_CHECK(std::get<1>(out.second[0].second[1])==0);
+  BOOST_CHECK(std::get<2>(out.second[0].second[1])==1);
+  BOOST_CHECK(std::get<3>(out.second[0].second[1])==0);
+
 }
