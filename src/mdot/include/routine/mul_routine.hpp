0e78384285ae2194fdf3411a4ae47d007ceb309a
konex5 <fix@konex.dev>
2025-02-21 14:07:49 +0100

    wip

--- src/mdot/include/routine/mul_routine.hpp
+++ src/mdot/include/routine/mul_routine.hpp
@@ -17,22 +17,17 @@ void mul_mm_blocs_new(
     std::vector<std::tuple<t_index_t, m_index_t, m_index_t>> buildtarget) {
   const double alpha = 1., beta = 0.;
   for (auto &[target, lhs_it, rhs_it] : buildtarget) {
-    auto N = static_cast<std::size_t>(std::get<0>(lhs_blocs[lhs_it].first));
-    auto M = static_cast<std::size_t>(std::get<2>(rhs_blocs[rhs_it].first));
-    auto K = static_cast<std::size_t>(std::get<1>(lhs_blocs[lhs_it].first) *
+    auto N = static_cast<std::size_t>(std::get<0>(lhs_blocs[lhs_it].first) *
                                       std::get<1>(lhs_blocs[lhs_it].first));
+    auto M = static_cast<std::size_t>(std::get<1>(rhs_blocs[rhs_it].first) *
+                                      std::get<2>(rhs_blocs[rhs_it].first));
+    auto K = static_cast<std::size_t>(std::get<2>(lhs_blocs[lhs_it].first));
     std::vector<dnum_t> mat_out(N * M);
 
     dgemm_((char *)"N", (char *)"N", &M, &N, &K, &alpha,
            rhs_blocs[rhs_it].second.data(), &M, lhs_blocs[lhs_it].second.data(),
            &K, &beta, mat_out.data(), &M);
-    /*
-        new_blocks[target] = _np.tensordot(
-            old_blocks1[it1],
-            old_blocks2[it2],
-            axes=(2, 0),
-        )
-    */
+
     new_blocs[target] = {{std::get<0>(lhs_blocs[lhs_it].first),
                           std::get<1>(lhs_blocs[lhs_it].first),
                           std::get<1>(rhs_blocs[rhs_it].first),
