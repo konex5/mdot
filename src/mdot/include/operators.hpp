c43ad9339809a8101ecbbe8593176998a778c12f
konex5 <fix@konex.dev>
2025-02-21 14:07:48 +0100

    add test_operators and rename function

--- src/mdot/include/operators.hpp
+++ src/mdot/include/operators.hpp
@@ -5,33 +5,39 @@
 
 #include "mdot/include/babel_type.hpp"
 
-std::pair<dopbloc_t, dnum_t> real_single_operator(std::string name,
+std::pair<dopbloc_t, dnum_t> single_operator_real(std::string name,
                                                   std::string qbasis) {
   std::map<std::string, typename std::map<std::string, dopbloc_t>>
-      real_single_operators;
+      single_operators_real;
   std::map<std::string, typename std::map<std::string, double>> normalization;
   // C mode
   normalization["sh-id"]["sh-none"] = 1. / sqrt(2);
-  real_single_operators["sh-id"]["sh-none"][{0, 0}] = {1, 0, 0, 1};
+  single_operators_real["sh-id"]["sh-none"][{0, 0}] = {1, 0, 0, 1};
   //
-  real_single_operators["sh-id"]["sh-u1"][{0, 0}] = {1};
-  real_single_operators["sh-id"]["sh-u1"][{1, 1}] = {1};
+  single_operators_real["sh-id"]["sh-u1"][{0, 0}] = {1};
+  single_operators_real["sh-id"]["sh-u1"][{1, 1}] = {1};
   ///
   normalization["sh-sp"]["sh-none"] = 0.;
-  real_single_operators["sh-sp"]["sh-none"][{0, 0}] = {0, 1, 0, 0};
+  single_operators_real["sh-sp"]["sh-none"][{0, 0}] = {0, 1, 0, 0};
   //
-  real_single_operators["sh-sp"]["sh-u1"][{0, 1}] = {1};
+  single_operators_real["sh-sp"]["sh-u1"][{0, 1}] = {1};
   ///
   normalization["sh-sm"]["sh-none"] = 0.;
-  real_single_operators["sh-sm"]["sh-none"][{0, 0}] = {0, 0, 1, 0};
+  single_operators_real["sh-sm"]["sh-none"][{0, 0}] = {0, 0, 1, 0};
   //
-  real_single_operators["sh-sm"]["sh-u1"][{1, 0}] = {1};
+  single_operators_real["sh-sm"]["sh-u1"][{1, 0}] = {1};
   //
   normalization["sh-sz"]["sh-none"] = 1. / sqrt(2);
-  real_single_operators["sh-sz"]["sh-none"][{0, 0}] = {1, 0, 0, -1};
+  single_operators_real["sh-sz"]["sh-none"][{0, 0}] = {1, 0, 0, -1};
   //
-  real_single_operators["sh-sz"]["sh-u1"][{0, 0}] = {1};
-  real_single_operators["sh-sz"]["sh-u1"][{1, 1}] = {-1};
+  single_operators_real["sh-sz"]["sh-u1"][{0, 0}] = {1};
+  single_operators_real["sh-sz"]["sh-u1"][{1, 1}] = {-1};
   //
-  return {real_single_operators[name][qbasis], normalization[name][qbasis]};
+  normalization["sh-sx"]["sh-none"] = 1. / sqrt(2);
+  single_operators_real["sh-sx"]["sh-none"][{0, 0}] = {0, 1, 1, 0};
+  //
+  single_operators_real["sh-sx"]["sh-u1"][{0, 1}] = {1};
+  single_operators_real["sh-sx"]["sh-u1"][{1, 0}] = {1};
+  //
+  return {single_operators_real[name][qbasis], normalization[name][qbasis]};
 }
--- tests/unit/test_operators.cpp
+++ tests/unit/test_operators.cpp
@@ -15,10 +15,10 @@ inline darr_t mul_twotwo(darr_t a, darr_t b) {
   return c;
 }
 
-BOOST_AUTO_TEST_CASE(test_operators) {
+BOOST_AUTO_TEST_CASE(test_operators_sh_none_real) {
 
-  auto shid = std::get<0>(real_single_operator("sh-id", "sh-none"));
-  auto shid_norm = std::get<1>(real_single_operator("sh-id", "sh-none"));
+  auto shid = std::get<0>(single_operator_real("sh-id", "sh-none"));
+  auto shid_norm = std::get<1>(single_operator_real("sh-id", "sh-none"));
 
   BOOST_CHECK((shid[{0, 0}])[0 + 0 * 2] == 1);
   BOOST_CHECK((shid[{0, 0}])[1 + 0 * 2] == 0);
@@ -35,15 +35,15 @@ BOOST_AUTO_TEST_CASE(test_operators) {
                            pow(shid_norm, 2) -
                        1) < 1e-7);
   //
-  auto shsp = std::get<0>(real_single_operator("sh-sp", "sh-none"));
+  auto shsp = std::get<0>(single_operator_real("sh-sp", "sh-none"));
   BOOST_CHECK((shsp[{0, 0}])[1 + 0 * 2] == 1);
-  auto shsm = std::get<0>(real_single_operator("sh-sm", "sh-none"));
+  auto shsm = std::get<0>(single_operator_real("sh-sm", "sh-none"));
   BOOST_CHECK((shsm[{0, 0}])[0 + 1 * 2] == 1);
   //
   auto shspsm = mul_twotwo(shsp[{0, 0}], shsm[{0, 0}]);
   BOOST_CHECK(shspsm[0 + 0 * 2] == 1);
   //
-  auto shsz = std::get<0>(real_single_operator("sh-sz", "sh-none"));
+  auto shsz = std::get<0>(single_operator_real("sh-sz", "sh-none"));
 
   BOOST_CHECK((shsz[{0, 0}])[0 + 0 * 2] == 1);
   BOOST_CHECK((shsz[{0, 0}])[1 + 0 * 2] == 0);
@@ -59,4 +59,25 @@ BOOST_AUTO_TEST_CASE(test_operators) {
   BOOST_CHECK(std::abs((shsz_square[0 + 0 * 2] + shsz_square[1 + 1 * 2]) *
                            pow(shid_norm, 2) -
                        1) < 1e-7);
+  //
+  /*
+  auto shsx = std::get<0>(single_operator_real("sh-sx", "sh-none"));
+
+  BOOST_CHECK((shsx[{0, 0}])[0 + 0 * 2] == 1);
+  BOOST_CHECK((shsx[{0, 0}])[1 + 0 * 2] == 0);
+  BOOST_CHECK((shsx[{0, 0}])[1 + 1 * 2] == -1);
+  BOOST_CHECK((shsx[{0, 0}])[0 + 1 * 2] == 0);
+
+  auto shsx_square = mul_twotwo(shid[{0, 0}], shid[{0, 0}]);
+  BOOST_CHECK(shsx_square[0 + 0 * 2] == 0);
+  BOOST_CHECK(shsx_square[1 + 0 * 2] == 1);
+  BOOST_CHECK(shsx_square[0 + 1 * 2] == 1);
+  BOOST_CHECK(shsx_square[1 + 1 * 2] == 0);
+
+  BOOST_CHECK(std::abs((shsx_square[0 + 0 * 2] + shsx_square[1 + 1 * 2]) *
+                           pow(shid_norm, 2) -
+                       1) < 1e-7);
+*/
 }
+
+BOOST_AUTO_TEST_CASE(test_operators_sh_none_cplx) {}
