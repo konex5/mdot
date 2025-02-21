05acb45c621744b6af41bfd9eca6445bd140fd9f
konex5 <fix@konex.dev>
2025-02-21 14:07:48 +0100

    add test_operators

--- src/mdot/include/operators.hpp
+++ src/mdot/include/operators.hpp
@@ -1,16 +1,26 @@
 
-#include <vector>
 #include <map>
 #include <string>
+#include <vector>
 
 #include "mdot/include/babel_type.hpp"
 
 dopbloc_t real_single_operator(std::string name, std::string qbasis) {
-    std::map < std::string, typename std::map<std::string, dopbloc_t>> real_single_operators;
-    std::map < std::string, typename std::map<std::string, double>> normalization;
+  std::map<std::string, typename std::map<std::string, dopbloc_t>>
+      real_single_operators;
+  std::map<std::string, typename std::map<std::string, double>> normalization;
+  // C mode
+  normalization["sh-id"]["sh-none"] = 1. / sqrt(2);
+  real_single_operators["sh-id"]["sh-none"][{0, 0}] = {1, 0, 0, 1};
+  //
+  real_single_operators["sh-id"]["sh-u1"][{0, 0}] = {1};
+  real_single_operators["sh-id"]["sh-u1"][{1, 1}] = {1};
+  ///
+  normalization["sh-sp"]["sh-none"] = 0.;
+  real_single_operators["sh-sp"]["sh-none"][{0, 0}] = {0, 1, 0, 0};
+  //
+  real_single_operators["sh-sp"]["sh-u1"][{0, 1}] = {1};
+  //
 
-    real_single_operators["sh-none"]["sh-id"][{0,0}] = {1,0,0,1}; 
-    normalization["sh-none"]["sh-id"] = 1./sqrt(2);
-    return real_single_operators[name][qbasis];
+  return real_single_operators[name][qbasis];
 }
-
--- tests/unit/test_operators.cpp
+++ tests/unit/test_operators.cpp
@@ -3,10 +3,27 @@
 #include "mdot/include/babel_type.hpp"
 #include "mdot/include/operators.hpp"
 
-BOOST_AUTO_TEST_CASE(test_indexdw)
-{
+inline darr_t mul_twotwo(darr_t a, darr_t b) {
+  darr_t c;
+  for (int i = 0; i < 2; i++)
+    for (int j = 0; j < 2; j++)
+      for (int k = 0; k < 2; k++)
+        c[i + 2 * j] = a[i + 2 * k] * b[k + 2 * j];
+  return c;
+}
 
-    auto a = real_single_operator("hello", "world");
-    
-    BOOST_CHECK((a[{0,0}])[0] == 1);
+BOOST_AUTO_TEST_CASE(test_indexdw) {
+
+  auto shid = real_single_operator("sh-id", "sh-none");
+
+  BOOST_CHECK((shid[{0, 0}])[0 + 0 * 2] == 1);
+  BOOST_CHECK((shid[{0, 0}])[1 + 0 * 2] == 0);
+  BOOST_CHECK((shid[{0, 0}])[1 + 1 * 2] == 1);
+  BOOST_CHECK((shid[{0, 0}])[0 + 1 * 2] == 0);
+
+  auto shid_square = mul_twotwo(shid[{0, 0}], shid[{0, 0}]);
+  BOOST_CHECK(shid_square[0 + 0 * 2] == 1);
+  BOOST_CHECK(shid_square[1 + 0 * 2] == 0);
+  BOOST_CHECK(shid_square[1 + 1 * 2] == 1);
+  BOOST_CHECK(shid_square[0 + 1 * 2] == 0);
 }
