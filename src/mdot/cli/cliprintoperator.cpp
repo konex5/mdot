100f1d085cd7fc1b18fe4ad8f35f48b17304d81f
konex5 <fix@konex.dev>
2025-02-21 14:07:48 +0100

    add printoperator cli

---
+++ src/mdot/cli/cliprintoperator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "mdot/include/operators_static.hpp"
+
+int main() {
+  // auto arr = mdot::real_sh_operators_crtp<mdot::sh_id_no>::array;
+  // auto shape = mdot::real_sh_operators_crtp<mdot::sh_id_no>::shape;
+  // auto size = mdot::real_sh_operators_crtp<mdot::sh_id_no>::size;
+
+  auto arr = mdot::cplx_sh_operators_crtp<mdot::sh_id_cplx_no>::array;
+  auto shape = mdot::cplx_sh_operators_crtp<mdot::sh_id_cplx_no>::shape;
+  auto size = mdot::cplx_sh_operators_crtp<mdot::sh_id_cplx_no>::size;
+
+  std::cout << "the matrix is..." << std::endl;
+  std::size_t nb_col = std::get<1>(shape);
+  for (std::size_t i = 0; i < std::get<0>(shape); i++) {
+    for (std::size_t j = 0; j < nb_col; j++)
+      std::cout << arr[j + i * nb_col] << " ";
+    std::cout << std::endl;
+  }
+  std::cout << "yeah!" << std::endl;
+
+  return 0;
+}
--- src/mdot/include/babel_type.hpp
+++ src/mdot/include/babel_type.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <array>
+#include <map>
 #include <complex>
 #include <vector>
 
